503e0663e4c7ad01c98cd1a1f6993c146a70a415
Hiroki Osame <hiroki.osame@gmail.com>
2021-03-25 17:33:11 -0400

    built

--- dist/pkg-size.js
+++ dist/pkg-size.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});var _chunkTR3WRLLOjs = require('./chunk-TR3WRLLO.js');exports.default = _chunkTR3WRLLOjs.d;
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});var _chunk7N66BF3Cjs = require('./chunk-7N66BF3C.js');exports.default = _chunk7N66BF3Cjs.d;
